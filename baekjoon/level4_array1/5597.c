858c0832b398bfffc1f96d0b0a3161666de857b0
mikeylim <mikey3437@gmail.com>
2023-06-05 23:56:04 -0400

    solved level4_array1/5597.c

---
+++ baekjoon/level4_array1/5597.c
@@ -0,0 +1,28 @@
+// Who have not handed in ? 다국어
+// 시간 제한	메모리 제한	제출	정답	맞힌 사람	정답 비율
+// 1 초	128 MB	68193	36139	31875	53.388%
+
+// # 문제
+// Professor M of JOI University teaches a course on computer programming.
+// 30 students take the course and are numbered 1 to 30.
+// 28 students have handed in their homework but the other two students have not yet.
+// Write a program which, given the numbers of students who have handed in their homework,
+// outputs the numbers of the students who have not handed in their homework.
+
+// 입력
+// The input file contains 28 lines.
+// Each line of the input file contains a number of a student who has handed in her/his homework.
+// The numbers in the input file are between 1 and 30, inclusive, and distinct from each other. The numbers are given in no particular order.
+
+// 출력
+// You should submit the output file which consists of two lines,
+// and each of lines should contain the number of a student who has not handed in her/his homowork.
+// The number in the first line should be less than the one in the second line.
+
+#include <stdio.h>
+
+int main() {
+    int x = [];
+    
+    return 0;
+}
