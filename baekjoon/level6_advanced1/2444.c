ca9bbce81f3c67082455db30a0b34b835aca857b
mikeylim <mikey3437@gmail.com>
2023-06-15 12:48:59 -0400

    modified solution: baekjoon/level6_advanced1/2444.c

--- baekjoon/level6_advanced1/2444.c
+++ baekjoon/level6_advanced1/2444.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 
 int main() {
-    int numStars;
-    scanf("%d", &numStars);
+    int n;
+    scanf("%d", &n);
 
-    // each line until line number of numStars
-    for (int i = 1; i <= numStars; i++) {
+    // each line until line number of n
+    for (int i = 1; i <= n; i++) {
         // top spaces
-        for (int j = numStars - 1; j >= 0; j--) {
-            // printf("\nj: %d\n", j);
-            printf(" ");
-        }
-        for (int k = 1; k <= 2*i-1; k++) {
+        for (int j = n - i; j > 0; j--) {
+		    printf(" ");
+		}
+        for (int k = 1; k <= 2 * i - 1; k++) {
             printf("*");
         }
+        
         printf("\n");
     }
+    for (int i = n - 1; i > 0; i--) {
+        for (int j = n - i; j > 0; j--) {
+            printf(" ");
+        }
+        for (int k = 0; k < 2 * i -1; k++) {
+            printf("*");
+        }
+        // for (int k = 2 * i - 1; k > 0; k--) {
+        //     printf("*");
+        // }
+        printf("\n");
+    }
+    
     
 }
