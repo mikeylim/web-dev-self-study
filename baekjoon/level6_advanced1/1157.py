e482b15d9fddb0d6d4584c0adac095cb6717ab5e
mikeylim <mikey3437@gmail.com>
2023-06-22 09:46:19 -0400

    solved baekjoon/level6_advanced1/1157.py

---
+++ baekjoon/level6_advanced1/1157.py
@@ -0,0 +1,44 @@
+# 단어 공부
+
+# 문제
+# 알파벳 대소문자로 된 단어가 주어지면, 이 단어에서 가장 많이 사용된 알파벳이 무엇인지 알아내는 프로그램을 작성하시오. 단, 대문자와 소문자를 구분하지 않는다.
+
+# 입력
+# 첫째 줄에 알파벳 대소문자로 이루어진 단어가 주어진다. 주어지는 단어의 길이는 1,000,000을 넘지 않는다.
+
+# 출력
+# 첫째 줄에 이 단어에서 가장 많이 사용된 알파벳을 대문자로 출력한다. 단, 가장 많이 사용된 알파벳이 여러 개 존재하는 경우에는 ?를 출력한다.
+
+
+# # using dictionary
+word = input().upper()
+if len(word) > 1000000:
+    print('wrong')
+else:    
+    wordList = list(word)
+    dic = {}
+
+    for alphabet in wordList:
+        dic[alphabet] = word.count(alphabet)
+
+    max_value = max(dic.values())
+    max_keys = [k for k,v in dic.items() if v == max_value]
+    if len(max_keys) > 1:
+        print('?')
+    else:
+        print(max_keys[0])
+
+# 답안지
+words = input().upper()
+unique_words = list(set(words))  # 입력받은 문자열에서 중복값을 제거
+
+cnt_list = []
+for x in unique_words :
+    cnt = words.count(x)
+    cnt_list.append(cnt)  # count 숫자를 리스트에 append
+
+if cnt_list.count(max(cnt_list)) > 1 :  # count 숫자 최대값이 중복되면
+    print('?')
+else :
+    max_index = cnt_list.index(max(cnt_list))  # count 숫자 최대값 인덱스(위치)
+    print(unique_words[max_index])
