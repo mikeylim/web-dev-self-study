f148fe8a955b1d59b231fea7524707a77b9da67d
mikeylim <mikey3437@gmail.com>
2023-07-01 23:59:46 -0400

    solved 10988.c

---
+++ baekjoon/level6_advanced1/10988.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_LENGTH 100
+
+int main() {
+    char str[MAX_LENGTH];
+    scanf("%s", str);
+    printf("%s", str);
+
+    for (int i = 0; i <= strlen(str) / 2; i++) {
+        
+    }
+}
