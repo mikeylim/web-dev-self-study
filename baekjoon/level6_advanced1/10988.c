61fd1bd20fc3539b92a6409b53a5fb45c5743f71
mikeylim <mikey3437@gmail.com>
2023-07-24 23:35:32 -0400

    modified solution: baekjoon/level6_advanced1/10988.c

--- baekjoon/level6_advanced1/10988.c
+++ baekjoon/level6_advanced1/10988.c
@@ -2,12 +2,26 @@
 #include <string.h>
 #define MAX_LENGTH 100
 
-int main() {
+
+int isPalindrome() {
     char str[MAX_LENGTH];
     scanf("%s", str);
-    printf("%s", str);
-
+    int isPalindrome;
     for (int i = 0; i <= strlen(str) / 2; i++) {
-        
+        if (str[i] == str[strlen(str) - i - 1]) {
+            isPalindrome = 1;
+        } else {
+            return 0;
+        }
     }
+    return isPalindrome;
 }
+
+int main() {
+    
+    printf("%d", isPalindrome());
+    
+    return 0;
+    
+}
+
