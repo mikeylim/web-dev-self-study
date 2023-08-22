633c93202652957b99766d24104b369d3e5eebe9
mikeylim <mikey3437@gmail.com>
2023-08-21 23:56:55 -0400

    solved baekjoon/10810.py

---
+++ baekjoon/10810.py
@@ -0,0 +1,13 @@
+n, m = input().split()
+arr = []
+
+for x in range(int(m)):
+    x = list(map(int, input().split()))
+    i = x[0]
+    j = x[1]
+    k = x[2]
+
+    while (i <= j):
+        arr
+        i += 1
+        print(arr)
